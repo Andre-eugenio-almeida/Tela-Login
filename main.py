4c837b5dc2e9c957dcc1bfa4ffabf16937aa11df
André Almeida <almeida.ae@outlook.com>
2023-03-10 14:29:30 -0300

    TELA/LOGIN

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import Tk, ttk
+
+#----------Cores---------------
+cor0 = "#f0f3f5"  #black
+cor1 = "#feffff"  #withe
+cor2 = "#3fb5a3"  #green
+cor3 = "#38576b"  #valor
+cor4 = "#403d3d"  #letra
+
+#--------frame----------------
+janela = Tk()
+janela.title('')
+janela.geometry('310x300')
+janela.configure(background=cor1)
+janela.resizable(width=FALSE, height=FALSE)
+
+janela.mainloop()
+
+
